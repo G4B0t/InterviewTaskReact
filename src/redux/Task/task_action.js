107911893c112b23f6d711771032b662246afec9
G4B0t <gabrielefrain23@gmail.com>
2024-04-17 15:48:19 -0400

    Testing code for create api from redux toolkit

--- src/redux/Task/task_action.js
+++ src/redux/Task/task_action.js
@@ -1,2 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 export const DATA_REQUESTED = "DATA_REQUESTED";
 export const DATA_RECEIVED = "DATA_RECEIVED";
+
+// Define the API slice using a constant for clarity
+const contactsApi = createApi({
+  reducerPath: "tasks",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/" }),
+  endpoints: (builder) => ({
+    contacts: builder.query({
+      query: () => "/tasks",
+    }),
+    contact: builder.query({
+      query: (id) => `/tasks/${id}`,
+    }),
+  }),
+});
+
+// Export the hooks for easy access
+export const { useContactsQuery, useContactQuery } = contactsApi;
